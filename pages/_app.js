c8041f25171cb70bc18b5f0fb70fb250ca6b0770
vaxenvixen <katiekim.aram@gmail.com>
2022-10-11 20:37:52 -0500

    index and layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,51 @@
-import '../styles/globals.css'
+import '../styles/globals.sass'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <html lang="en">
+      <head>
+        <title>Katie Kim</title>
+
+      </head>
+
+      <nav>
+        <div>
+          <button><i data-feather="play"></i></button>
+          <button id="darkness_my_old_friend"><i data-feather="moon"></i></button>
+        </div>      
+        <div>
+          <a href="#about">About Me</a>
+          <a href="#exp">Experience</a>
+          <a href="#projects">Projects</a>
+          <a>Contact</a>
+          <a href="https://katiekimdev.medium.com">Blog</a>
+          <a href="/download/resume.pdf" class="resume">Resume</a>
+        </div>
+      </nav>
+
+      <main>
+        <Component {...pageProps} />
+      </main>
+
+      <footer>
+        <div>
+          <span>austin, tx</span>
+        </div>
+        <div>
+          <span><a href="mailto:me@katiekim.dev" class="mail">me@katiekim.dev</a></span>
+        </div>
+        <div>
+          <a href="" class="social"><i data-feather="github"></i></a>
+          <a href="" class="social"><i data-feather="linkedin"></i></a>
+          <a href="" class="social"><i data-feather="instagram"></i></a>
+          <a href="" class="social"><i data-feather="twitter"></i></a>
+          <a href="" class="social"><i data-feather="codepen"></i></a>
+        </div>
+      </footer>
+
+      <script>feather.replace()</script>
+    </html>
+  )
 }
 
 export default MyApp

563211edb489f1cec28a2e135e68ccbfa28221ec
vaxenvixen <katiekim.aram@gmail.com>
2022-10-13 20:59:41 -0500

    adding components

---
+++ pages/project.js
@@ -0,0 +1,19 @@
+import styles from '../styles/Experience.module.sass'
+import Image from 'next/image'
+
+import React from 'react';
+import * as Icon from 'react-feather';
+
+export default function Experience() {
+  return (
+    <article className={styles.exp}>
+        <div>
+        {/* What I've built
+
+            All projects
+            Should render all github projects and codepen.io by date
+            View archive */}
+        </div>
+    </article>
+  )
+}
